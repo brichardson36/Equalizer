62a401c2ee8e251f5a9fdd8a0004d65b0fac14bf
rfaulkner7 <rfaulkner7@gatech.edu>
2021-03-31 15:25:54 -0400

    frontend

---
+++ frontend/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  render(){
+    return (
+      // <View style = {styles.container}>
+      //   <Login></Login>
+      // </View>
+      <NavigationContainer style = {styles.container}>
+        <Stack.Navigator initialRouteName = "Login">
+          <Stack.Screen name = "Login" component = {Login}></Stack.Screen>
+          <Stack.Screen name = "Home" component = {Home}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
