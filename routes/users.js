2eb894c05b2acf9e3fdeb405e154e6fc8df14347
jiayi42 <misspog42@gmail.com>
2021-04-02 15:47:28 -0400

    Add files via upload
    remove unnecessary content in schema so change some register and post for users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
-
 router.get(`/`, async (req, res) =>{
     const userList = await User.find().select("name phone email");
 
@@ -28,12 +27,7 @@ router.post(`/`, async (req,res) =>{
         email: req.body.email,
         passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
+        isAdmin: req.body.isAdmin
     })
     user = await user.save();
 
@@ -77,12 +71,7 @@ router.post(`/register`, async (req,res) =>{
         email: req.body.email,
         passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
+        isAdmin: req.body.isAdmin
     })
     user = await user.save();
 
