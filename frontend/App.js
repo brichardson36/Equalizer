974d11f55a8e640e51028124ef0b15d9b93deab6
nazanin-tabatabaei <nazanin.tbt@gatech.edu>
2021-04-03 18:48:11 -0400

    materialbottom bar

--- frontend/App.js
+++ frontend/App.js
@@ -1,64 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import Login from "./Pages/Login";
-import Home from "./Pages/Home";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Icon from 'react-native-vector-icons/Ionicons';
 
-const HomeStack = createStackNavigator();
-const LoginStack = createStackNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import MainTabScreen from "./Pages/MainTabScreen";
 const Drawer = createDrawerNavigator();
 
-const HomeStackScreen=({navigation})=>(
-  <HomeStack.Navigator  
-  screenOptions={{
-    headerStyle:{
-      backgroundColor: '#009387',
-    },
-    headerTintColor:'#fff',
-    headerTitleStyle:{
-      fontWeight:'bold',
-      //alignSelf: 'center',
-    },
-  }}
-  >
-  
-    <HomeStack.Screen name = "Home" component = {Home} options={{
-    headerLeft:()=>( 
-      <Icon.Button name="ios-menu" size={25}
-      backgroundColor= '#009387'
-      onPress={()=>navigation.openDrawer()}></Icon.Button>
-    )
-    }}/>
-  </HomeStack.Navigator>
-);
 
-const LoginStackScreen=({navigation})=>(
-  <LoginStack.Navigator  
-  screenOptions={{
-    headerStyle:{
-      backgroundColor: '#009387',
-    },
-    headerTintColor:'#fff',
-    headerTitleStyle:{
-      fontWeight:'bold',
-      //alignSelf: 'center',
-    },
-  }}
-  >
-  
-    <LoginStack.Screen name = "Login" component = {Login}options={{
-    headerLeft:()=>( 
-      <Icon.Button name="ios-menu" size={25}
-      backgroundColor= '#009387'
-      onPress={()=>navigation.openDrawer()}></Icon.Button>
-    )
-    }}/>
-  </LoginStack.Navigator>
-);
 
 export default class App extends React.Component {
   render(){
@@ -68,8 +18,8 @@ export default class App extends React.Component {
       // </View>
       <NavigationContainer style = {styles.container}>
         <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-        <Drawer.Screen name="Login" component={LoginStackScreen} />
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        {/*<Drawer.Screen name="Login" component={LoginStackScreen} />*/}
         </Drawer.Navigator>
 
       </NavigationContainer>
