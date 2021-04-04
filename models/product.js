e7224f6f2199529dcf1884be6bde8cb68ee89de9
jiayi42 <misspog42@gmail.com>
2021-04-03 22:58:47 -0400

    Add files via upload
    fit the fake store

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: {
+    title: {
         type: String,
         required: true,
     },
@@ -9,47 +9,24 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    richDescription: {
-        type: String,
-        default: ''
-    },
+ 
     image: {
         type: String,
         default: ''
     },
-    images: [{
-        type: String
-    }],
-    brand: {
-        type: String,
-        default: ''
-    },
+ 
     price : {
         type: Number,
         default:0
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
+        type: String,//mongoose.Schema.Types.ObjectId,
+        //ref: 'Category',
         required:true
     },
-    countInStock: {
+    fid : {
         type: Number,
-        required: true,
-        min: 0,
-        max: 255
-    },
-    rating: {
-        type: Number,
-        default: 0,
-    },
-    numReviews: {
-        type: Number,
-        default: 0,
-    },
-    isFeatured: {
-        type: Boolean,
-        default: false,
+        default:0
     },
     dateCreated: {
         type: Date,
