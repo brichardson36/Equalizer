797ef3d8fd5c4240c92479aa8bc096d203470eec
rfaulkner7 <rfaulkner7@gatech.edu>
2021-04-30 20:46:20 -0400

    token gen

--- frontend/Pages/Home.js
+++ frontend/Pages/Home.js
@@ -1,6 +1,13 @@
 import React, {useEffect} from 'react';
 import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { AuthContext } from '../AppScreen';
+import { Searchbar ,Card } from 'react-native-paper';
+import { Button } from 'react-native-elements';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+import Expo from 'expo';
+
 import {
     Avatar,
     Title,
@@ -10,13 +17,45 @@ import {
     TouchableRipple,
     Switch
 } from 'react-native-paper';
+import {useTheme} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
 
-export default function Home() {
-
+export default function Home({navigation}) {
+    const{colors}=useTheme();
     useEffect(()=> {
+        registerForPushNotificationsAsync();
         getContent();
     }, [])
 
+    registerForPushNotificationsAsync = async () => {
+        if (Constants.isDevice) {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+          if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+          if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+          }
+          const token = (await Notifications.getExpoPushTokenAsync()).data;
+          console.log(token);
+          this.setState({ expoPushToken: token });
+        } else {
+          alert('Must use physical device for Push Notifications');
+        }
+      
+        if (Platform.OS === 'android') {
+          Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+          });
+        }
+        };
+        
     const[imgContent, setImgContent] = React.useState('none')
     const[description, setDescription] = React.useState('none')
     const[price, setPrice] = React.useState('none')
@@ -36,50 +75,175 @@ export default function Home() {
 
     return(
         <View style = {{flex: 1, flexDirection: "column"}}>
-            <View style = {{flex: 1, flexDirection: "row", paddingTop:30}}>
-                <View>
-                    <Image source={{ uri: imgContent }} style={{ width: 150, height: 150 }} />
-                </View>
-                <View style = {{ flex: 1, flexDirection: "column"}}>
-                    <View style = {{ flex: 1, paddingLeft: 15 }}>
-                        <Text>{description}</Text>
-                    </View>
-                </View>
-            </View>
-            <View style = {{flex: 1, marginTop: 20}}>
-            <TouchableRipple onPress={()=>{toggleTrack()}}>
-                <View style={styles.preference}>
-                    <Text>${price} - Track?</Text>
-                    <View pointerEvents="none">
-                        <Switch value={track}/>
-                    </View>
-                    </View>
-            </TouchableRipple>
-            </View>
+            {/*<StatusBar barStyle={theme.dark? "light-content":"dark-content"}/>*/}
+            <StatusBar barStyle="light-content"/>
+            <Searchbar
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+            />
+
+
+        <View style = {{flex: 0, paddingTop:20,paddingBottom:5}}>
+            <View style = { styles.scrollViewHolder }>
+                <ScrollView horizontal = { true } showsHorizontalScrollIndicator = { false }>
+                    <Text style = { styles.item }>Electronics</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Software</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Clothes</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Automotive</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Toys and Games</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Tools</Text>
+                    <View style = { styles.separator }/>
+                    <Text style = { styles.item }>Household</Text>
+                </ScrollView>
+            </View>             
+        </View>
+        
+
+        <ScrollView
+          style={{
+            flexGrow: 0,
+            width: "100%",
+            height: "100%",
+        }}>
+            <View style={styles.row1}>
             
-
-            <View style = {{flex: 1, flexDirection: "row"}}>
-                <View>
-                    <Image source={{ uri: imgContentTwo }} style={{ width: 150, height: 150 }} />
+                <View style = {{flex: 1,padding:5,margin:5}}>
+                    <Card >
+                    <View style={{padding:5}}>
+                        <Text style={{marginBottom: 10, marginTop: 10, color: colors.text }} h2>
+                            BackPack
+                        </Text>
+                        <Image
+                        style={{ width: "100%", height: 150 }}
+                        resizeMode="cover"
+                        source={{ uri: imgContent }}
+                        />
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <View style = {{flex: 1}}>
+                                <Text style={styles.price} h4>
+                                    $ {price}
+                                </Text>
+                            
+                                <Text h6 style={styles.description}>
+                                    added 2h ago
+                                 </Text>
+                            </View>
+                        </View>
+                        <Button
+                        type="clear"
+                        title='Details'
+                        //onPress={() => this.props.navigation.navigate('Details')} 
+                        onPress={()=>navigation.navigate('DetailsScreen')}
+                        />
+                        </View>
+                    </Card>
                 </View>
-                <View style = {{ flex: 1, flexDirection: "column"}}>
-                    <View style = {{ flex: 1, paddingLeft: 15 }}>
-                        <Text>{descriptionTwo}</Text>
-                    </View>
+                <View style = {{flex: 1,padding:5,margin:5}}>
+                    <Card >
+                    <View style={{padding:5}}>
+                        <Text style={{marginBottom: 10, marginTop: 10, color: colors.text }} h2>
+                            BackPack
+                        </Text>
+                        <Image
+                        style={{ width: "100%", height: 150 }}
+                        resizeMode="cover"
+                        source={{ uri: imgContentTwo }}
+                        />
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <View style = {{flex: 1}}>
+                                <Text style={styles.price} h4>
+                                    $ {priceTwo}
+                                </Text>
+                            
+                                <Text h6 style={styles.description}>
+                                    added 2h ago
+                                 </Text>
+                            </View>
+                        </View>
+                        <Button
+                        type="clear"
+                        title='Details'
+                        //onPress={() => this.props.navigation.navigate('Details')} 
+                        onPress={()=>navigation.navigate('DetailsScreen')}
+                        />
+                        </View>
+                    </Card>
                 </View>
             </View>
-            <View style = {{flex: 1, marginTop: 50}}>
-            <TouchableRipple onPress={()=>{toggleTrackTwo()}}>
-                <View style={styles.preference}>
-                    <Text>${priceTwo} - Track?</Text>
-                    <View pointerEvents="none">
-                        <Switch value={trackTwo}/>
-                    </View>
-                    </View>
-            </TouchableRipple>
+
+
+            <View style={styles.row1}>
+            
+                <View style = {{flex: 1,padding:5,margin:5}}>
+                    <Card >
+                        <View style={{padding:5}}>
+                        <Text style={{marginBottom: 10, marginTop: 10, color: colors.text }} h2>
+                            BackPack
+                        </Text>
+                        <Image
+                        style={{ width: "100%", height: 150 }}
+                        resizeMode="cover"
+                        source={{ uri: imgContent }}
+                        />
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <View style = {{flex: 1}}>
+                                <Text style={styles.price} h4>
+                                    $ {price}
+                                </Text>
+                            
+                                <Text h6 style={styles.description}>
+                                    added 2h ago
+                                 </Text>
+                            </View>
+                        </View>
+                        <Button
+                        type="clear"
+                        title='Details'
+                        //onPress={() => this.props.navigation.navigate('Details')} 
+                        onPress={()=>navigation.navigate('DetailsScreen')}
+                        />
+                        </View>
+                    </Card>
+                </View>
+                <View style = {{flex: 1,padding:5,margin:5}}>
+                    <Card >
+                        <View style={{padding:5}}>
+                        <Text style={{marginBottom: 10, marginTop: 10, color: colors.text }} h2>
+                            BackPack
+                        </Text>
+                        <Image
+                        style={{ width: "100%", height: 150 }}
+                        resizeMode="cover"
+                        source={{ uri: imgContentTwo }}
+                        />
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                            <View style = {{flex: 1}}>
+                                <Text style={styles.price} h4>
+                                    $ {priceTwo}
+                                </Text>
+                            
+                                <Text h6 style={styles.description}>
+                                    added 2h ago
+                                 </Text>
+                            </View>
+                        </View>
+                        <Button
+                        type="clear"
+                        title='Details'
+                        //onPress={() => this.props.navigation.navigate('Details')} 
+                        onPress={()=>navigation.navigate('DetailsScreen')}
+                        />
+                        </View>
+                    </Card>
+                </View>
             </View>
-
-
+        </ScrollView>
 
         </View>
     )
@@ -89,7 +253,6 @@ export default function Home() {
             res => {
                 res.json().then(
                     data=>{
-                        console.log(data)
                         setImgContent(data.image)
                         setDescription(data.description)
                         setPrice(data.price)
